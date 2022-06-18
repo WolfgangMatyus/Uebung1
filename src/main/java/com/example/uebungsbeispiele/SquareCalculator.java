aeaa91e64ba36a3e63c989c18d0fb36b0330369a
Matyus Wolfgang <wi21b055@technikum-wien.at>
2022-06-18 19:57:44 +0200

    Built SquareCalculator class changed SquareController class

--- src/main/java/com/example/uebungsbeispiele/SquareCalculator.java
+++ src/main/java/com/example/uebungsbeispiele/SquareCalculator.java
@@ -3,10 +3,8 @@ package com.example.uebungsbeispiele;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 public class SquareCalculator {
 
-    @GetMapping("/square")
     public int square(int a) {
         return a * a;
     }
