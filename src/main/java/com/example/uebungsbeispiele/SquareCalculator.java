97c9da8717b1fad83b7d6892f4e93d5085de65c2
Matyus Wolfgang <wi21b055@technikum-wien.at>
2022-06-18 19:43:11 +0200

    Built SquareCalculator class changed SquareController class and built first Unit Test

--- src/main/java/com/example/uebungsbeispiele/SquareCalculator.java
+++ src/main/java/com/example/uebungsbeispiele/SquareCalculator.java
@@ -1,13 +1,7 @@
 package com.example.uebungsbeispiele;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class SquareCalculator {
-
-    @GetMapping("/square")
-    public int square(int a) {
+public class SquareCalculator{
+    public int square(int a){
         return a * a;
     }
 }
--- src/test/java/com/example/uebungsbeispiele/SquareCalculatorTest.java
+++ src/test/java/com/example/uebungsbeispiele/SquareCalculatorTest.java
@@ -1,2 +1,30 @@
-package com.example.uebungsbeispiele;public class SqareCalculatorTest {
+package com.example.uebungsbeispiele;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SquareCalculatorTest {
+
+    private SquareCalculator calcSquare;
+
+    @BeforeEach
+            void initialise() {calcSquare = new SquareCalculator();}
+
+    @Test
+    void square(){
+        // arrange
+        int a = 2;
+
+        // act
+        int result = calcSquare.square(a);
+
+        // assert
+        assertEquals(4, result);
+    }
+
 }
